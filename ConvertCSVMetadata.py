99095a8c1184f968c7d40ab7e3f0c0254d9c810d
konkeror <sid.mkjee@gmail.com>
2022-08-20 15:52:36 +0200

    Read mp3 metadata using eyed3

--- ConvertCSVMetadata.py
+++ ConvertCSVMetadata.py
@@ -1,2 +1,11 @@
 import eyed3
 
+audiofile = eyed3.load("pink.mp3")
+
+print("Title:", audiofile.tag.title)
+print("Artist:", audiofile.tag.artist)
+print("Album:", audiofile.tag.album)
+print("Album artist:", audiofile.tag.album_artist)
+print("Composer:", audiofile.tag.composer)
+print("Publisher:", audiofile.tag.publisher)
+print("Genre:", audiofile.tag.genre)
