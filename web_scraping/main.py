ca9adfc748f9d373e2f2c428fae947eba77238c5
konkeror <sid.mkjee@gmail.com>
2023-08-29 17:23:26 +0200

    Extract Text From HTML With String Methods

--- web_scraping/main.py
+++ web_scraping/main.py
@@ -8,3 +8,13 @@ print(page)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 print(html)
+
+# Extract Text From HTML With String Methods
+title_index = html.find("<title>")
+print(title_index)
+start_index = title_index + len("<title>")
+print(start_index)
+end_index = html.find("</title>")
+print(end_index)
+title = html[start_index:end_index]
+print(title)
